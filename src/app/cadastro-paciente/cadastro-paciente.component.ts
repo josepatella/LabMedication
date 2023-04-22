a7ae2bd9968150ddebd1aea087fdd0bbaf4cd892
Patella <jfpatella@gmail.com>
2023-04-22 19:04:49 -0300

    Armazenamento localStorage

--- src/app/cadastro-paciente/cadastro-paciente.component.ts
+++ src/app/cadastro-paciente/cadastro-paciente.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-paciente',
   templateUrl: './cadastro-paciente.component.html',
-  styleUrls: ['./cadastro-paciente.component.sass']
+  styleUrls: ['./cadastro-paciente.component.sass'],
 })
-export class CadastroPacienteComponent {
+export class CadastroPacienteComponent implements OnInit {
+  pacientesCadastrados: any[] = [];
+  Paciente: any = {
+    nomeCompleto: '',
+    sexo: '',
+    dataNascimento: '',
+    cpf: '',
+    rg: '',
+    telefone: '',
+    estadoCivil: '',
+    email: '',
+    naturalidade: '',
+    contatoEmergencia: '',
+    listaAlergias: '',
+    listaCuidados: '',
+    convenio: '',
+    numeroConvenio: '',
+    validadeConvenio: '',
+    cep: '',
+    cidade: '',
+    estado: '',
+    logradouro: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    pontoReferencia: '',
+  };
 
+  alerta = false;
+  constructor() {}
+  ngOnInit() {}
+
+  public cadastrar(f: NgForm) {
+    {
+      this.pacientesCadastrados.push(this.Paciente);
+      localStorage.setItem(
+        'pacientesCadastrados',
+        JSON.stringify(this.pacientesCadastrados)
+      );
+      this.Paciente = {
+        nomeCompleto: '',
+        sexo: '',
+        dataNascimento: '',
+        cpf: '',
+        rg: '',
+        telefone: '',
+        estadoCivil: '',
+        emil: '',
+        naturalidade: '',
+        contatoEmergencia: '',
+        listaAlergias: '',
+        listaCuidados: '',
+        convenio: '',
+        numeroConvenio: '',
+        validadeConvenio: '',
+        cep: '',
+        cidade: '',
+        estado: '',
+        logradouro: '',
+        numero: '',
+        complemento: '',
+        bairro: '',
+        pontoReferencia: '',
+      };
+      alert('Cadastro realizado com sucesso!');
+      this.alerta = true;
+    }
+  }
 }
