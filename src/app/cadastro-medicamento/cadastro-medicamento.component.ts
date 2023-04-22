8fb4313f167fb9d581cf3ad22784b366731bfcc1
Patella <jfpatella@gmail.com>
2023-04-22 19:48:28 -0300

    Armazenamento cadastro medicamentos localStorage

--- src/app/cadastro-medicamento/cadastro-medicamento.component.ts
+++ src/app/cadastro-medicamento/cadastro-medicamento.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-medicamento',
   templateUrl: './cadastro-medicamento.component.html',
   styleUrls: ['./cadastro-medicamento.component.sass']
 })
-export class CadastroMedicamentoComponent {
+export class CadastroMedicamentoComponent implements OnInit {
+  medicamentoPacienteCadastrados: any[] = [];
+  medicamentoPaciente: any = {
+    nomePaciente: '',
+    medicamento: '',
+    data: '',
+    hora: '',
+    tipo: '',
+    quantidade: '',
+    unidade: '',
+    observacao: '',
+  };
 
+  alerta = false;
+  constructor() {}
+  ngOnInit() {}
+
+  public cadastrar(f: NgForm) {
+    {
+      this.medicamentoPacienteCadastrados.push(this.medicamentoPaciente);
+      localStorage.setItem(
+        'pacientesCadastrados',
+        JSON.stringify(this.medicamentoPaciente)
+      );
+      this.medicamentoPaciente = {
+        nomePaciente: '',
+    medicamento: '',
+    data: '',
+    hora: '',
+    tipo: '',
+    quantidade: '',
+    unidade: '',
+    observacao: '',
+      };
+      alert('Cadastro realizado com sucesso!');
+      this.alerta = true;
+    }
+  }
 }
